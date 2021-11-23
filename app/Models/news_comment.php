f173d84ffc6d3f95fe858d294c28c8defab25ea9
XxTheFoxeeXx <71909607+deadly2014@users.noreply.github.com>
2021-11-23 21:35:59 +0700

    Update news_comment.php
    news detail

--- app/Models/news_comment.php
+++ app/Models/news_comment.php
@@ -13,16 +13,28 @@ class news_comment extends Model
     // use HasFactory;
     protected $guarded = ['id'];
 
-    public static function add($users_id, $news_id){
+    public static function add($users_id, $news_id, $comment){
         news_comment::create([
             "users_id" => $users_id,
-            "news_id" => $news_id
+            "news_id" => $news_id,
+            "comment" => $comment
         ]);
     }
 
     public static function getAll(){
         $news = DB::table('news_comments')->get();
         return $news;
+    }
+
+    public static function getComments($id){
+        $comments = DB::table('news_comments')
+            ->join('news', 'news_comments.news_id', '=', 'news.id')
+            ->join('users', 'news_comments.users_id', '=', 'users.id')
+            ->select('*')
+            ->where('news.id', $id)
+            ->get();
+
+        return $comments;
     }
 
     public static function deleteById($id){
